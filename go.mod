fa60f8ced19b600b768e18b91760400f6cbf2274
heartziq <heartziq@gmail.com>
2021-04-29 21:35:57 +0800

    Disallow registration empty passwords

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- handlers/register.go
+++ handlers/register.go
@@ -1,16 +1,49 @@
 package handlers
 
 import (
+	"database/sql"
 	"dental/helper"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 
 	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Register struct {
 	Tpl *template.Template
+}
+
+func createPassword(password string) []byte {
+	if hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost); err != nil {
+		log.Println(err)
+		return nil
+	} else {
+		return hash
+	}
+}
+
+func insertRecord(db *sql.DB, username, password string) int {
+	pwd := createPassword(password)
+	results, err := db.Exec("INSERT INTO MYSTOREDB.Users VALUES (?, ?)",
+
+		username, pwd)
+
+	if err != nil {
+
+		//panic(err.Error())
+		return 0
+
+	} else {
+
+		rows, _ := results.RowsAffected()
+
+		fmt.Println(rows)
+		return 1
+
+	}
 }
 
 func (reg *Register) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -33,23 +66,28 @@ func (reg *Register) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		pwd := r.FormValue("Password")
 		cPwd := r.FormValue("ConfirmPassword")
 
-		if pwd == cPwd {
-			// cookie, _ := r.Cookie("userInfo")
-			helper.AddUser(username, pwd)
-			// no cookie here - hence nil derefer
-			// set cookie
-			id := uuid.NewV4()
-			userCookie := &http.Cookie{
-				Name:  "userInfo",
-				Value: id.String(),
-			}
-
-			http.SetCookie(w, userCookie)
-			helper.UpdateSession(username, userCookie.Value)
-			http.Redirect(w, r, "/browse", http.StatusSeeOther)
+		if pwd == "" {
+			message = "Password cannot be empty"
 		} else {
-			message = "Password and Confirm Password do not match"
+			if pwd == cPwd {
+				// cookie, _ := r.Cookie("userInfo")
+				helper.AddUser(username, pwd)
+				// no cookie here - hence nil derefer
+				// set cookie
+				id := uuid.NewV4()
+				userCookie := &http.Cookie{
+					Name:  "userInfo",
+					Value: id.String(),
+				}
+
+				http.SetCookie(w, userCookie)
+				helper.UpdateSession(username, userCookie.Value)
+				http.Redirect(w, r, "/browse", http.StatusSeeOther)
+			} else {
+				message = "Password and Confirm Password do not match"
+			}
 		}
+
 	}
 
 	err := reg.Tpl.ExecuteTemplate(w, "index.gohtml", struct {
